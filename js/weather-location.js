80de80d1bf9e707434c304f8e1f123e868c1ccfd
Abhishek Prasad <abhishek71994@gmail.com>
2017-08-01 00:12:15 +0530

    problem with API

---
+++ js/weather-location.js
@@ -0,0 +1,25 @@
+
+	(function(){
+		'use strict';
+		var longitude,latitude;
+
+		function onPositionRecieved(position){
+			console.log(position);
+			longitude=position.coords.longitude;
+			latitude=position.coords.latitude;
+			var URL='https://api.darksky.net/forecast/6fc0fc8f313f6942e3988506f10117d5/'+latitude+','+longitude+'?units=si&exclude=[minutely,hourly,daily]';
+			$.getJSON(URL,function(data){
+				console.log(data);
+			});
+		}
+		function locationNotRecieved(positionErr){
+			console.log("Error"+positionErr)
+		}
+		if(navigator.geolocation){
+			navigator.geolocation.getCurrentPosition(onPositionRecieved,locationNotRecieved);
+
+		}
+	}());
+
+
+

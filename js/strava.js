0c5106bda04e9c52ebe687125db4d625d5ad963b
toni@tonigibbons.co.uk <toni@tonigibbons.co.uk>
2017-07-31 15:29:35 +0100

    displays multiple Strava activities and total distance

--- js/strava.js
+++ js/strava.js
@@ -3,14 +3,28 @@ const secondsInWeek = 7 * 24 * 60 * 60;
 var dateNow = Date.now() / 1000;
 var oneWeek = dateNow - secondsInWeek;
 var accessToken = "f6a9cab6e89b1dcd702561d809e77a84878e8925";
-//console.log(oneWeek);
 
 $.get('https://www.strava.com/api/v3/activities?access_token=' + accessToken + '&after=' + oneWeek, function(data) {
   console.log(data);
   var activityCount = data.length;
-  var activityType = data[0].type;
-  var distance = data[0].distance / 1000;
+  var activityType;
+  var distance;
+  var totalDistance = 0;
+  var activities = [];
+
+  for (var i = 0; i < activityCount; i++) {
+    activityType = data[i].type;
+    distance = data[i].distance;
+    totalDistance += distance;
+    activities.push([activityType, distance]);
+    distance = addUnits(activityType, distance);
+    $('#strava-data').append('<p>Activity: <span class="activityType">' + activityType + '</span> Distance: <span class="distance">' + distance + '</span></p>');
+
+  }
   $('#activityCount').html(activityCount);
-  $('#activityType').html(activityType);  
-  $('#distance').html(distance);
+  $('#strava-data').append('<p>Total Distance: <span class="totalDistance">' + (totalDistance / 1000).toFixed(2) + 'km</span></p>');
 });
+
+function addUnits(type, dist) {
+  return (dist / 1000).toFixed(2) + 'km';
+}

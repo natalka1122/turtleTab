89bb76f33289bf45714dc28b8222810828d19beb
Abhishek Prasad <abhishek71994@gmail.com>
2017-08-01 10:09:12 +0530

    Geo-location error

--- js/weather-location.js
+++ js/weather-location.js
@@ -2,18 +2,22 @@
 	(function(){
 		'use strict';
 		var longitude,latitude;
+		const proxyCors="https://cors-anywhere.herokuapp.com/";
+		const key='6fc0fc8f313f6942e3988506f10117d5';
 
 		function onPositionRecieved(position){
-			console.log(position);
+			console.log(position+"is fired!");
 			longitude=position.coords.longitude;
 			latitude=position.coords.latitude;
-			var URL='https://api.darksky.net/forecast/6fc0fc8f313f6942e3988506f10117d5/'+latitude+','+longitude+'?units=si&exclude=[minutely,hourly,daily]';
-			$.getJSON(URL,function(data){
+			
+			var url=proxyCors+'https://api.darksky.net/forecast/'+key+'/'+latitude+','+longitude+'?units=si&exclude=[minutely,hourly,daily]';
+			
+			$.getJSON(url,function(data){
 				console.log(data);
 			});
 		}
 		function locationNotRecieved(positionErr){
-			console.log("Error"+positionErr)
+			console.log("damn!"+positionErr)
 		}
 		if(navigator.geolocation){
 			navigator.geolocation.getCurrentPosition(onPositionRecieved,locationNotRecieved);

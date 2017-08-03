aa0b01047fa07249c341d3dc139032ef48737b90
toni@tonigibbons.co.uk <toni@tonigibbons.co.uk>
2017-08-03 02:17:26 +0100

    storing options to chrome.storage and retrieving, old method

---
+++ js/options.js
@@ -0,0 +1,36 @@
+// saves options to chrome.storage
+function save_options() {
+  var bgFrequency;
+  if (document.getElementById('bgHourly').checked) {
+    bgFrequency = 'hourly';
+  }
+  else {
+    bgFrequency = 'daily';
+  }
+  chrome.storage.sync.set({
+    bgFrequency: bgFrequency
+  }, function() {
+    // update status to let user know options were saved
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 2000);
+  });
+}
+
+// restores options settings using preferences stored in chrome.storage
+function restore_options() {
+  chrome.storage.sync.get('bgFrequency', function(items) {
+    if (items.bgFrequency == 'hourly') {
+      document.getElementById('bgHourly').setAttribute('checked', 'checked');
+    }
+    else {
+      document.getElementById('bgDaily').setAttribute('checked', 'checked');
+    }
+    
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
